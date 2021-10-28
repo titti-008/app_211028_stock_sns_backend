9f701a5278c7bcfc92416392b2d292d6b38f0fe2
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-10-29 07:12:00 +0900

    211029_fix_URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost\:\d+/
+    origins /localhost\:\d+/, /app-211028-react-frontend\.herokuapp\.com/
 
     resource '*',
       headers: :any,
