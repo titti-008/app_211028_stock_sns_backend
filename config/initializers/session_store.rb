2d630c2cd9e740c0ee318d6f5cf4cd5b39049754
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-07 20:33:07 +0900

    211107_2_fix_session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,6 @@
 if Rails.env === "production"
-  Rails.application.config.session_store :cookie_store, key: '_rails-backend', domain: "app-211028-react-frontend.herokuapp.com"
+  
+  Rails.application.config.session_store :cookie_store, key: '_rails-backend',expire_after:2.weeks, domain: "app-211028-react-frontend.herokuapp.com", tld_length: 2
 else
   Rails.application.config.session_store :cookie_store, key: '_rails-backend'
 end
