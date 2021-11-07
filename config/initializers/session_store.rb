dc47ab9de84e00653511418a61255d457f2de2dc
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-07 21:44:46 +0900

    fix session_store_domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env === "production"
   
-  Rails.application.config.session_store :cookie_store, key: '_rails-backend',domain: ".herokuapp.com/"
+  Rails.application.config.session_store :cookie_store, key: '_rails-backend',domain: ".herokuapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: '_rails-backend'
 end
