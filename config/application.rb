d4c6121fc4c242b29c1238924c1c179bd587cd8c
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-09 17:00:24 +0900

    211109_add_testLogin

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module RailsBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
-    config.middleware.use ActionDispatch::Cookies # 追加
-    config.middleware.use ActionDispatch::Session::CookieStore # 追加
+    # config.middleware.use ActionDispatch::Cookies # 追加
+    # config.middleware.use ActionDispatch::Session::CookieStore # 追加
     # config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware # うまくいかないので試しに追加
   end
 end
