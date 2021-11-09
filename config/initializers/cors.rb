da67c8fdf32c0b83dd800ede72402098e7074a51
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-09 12:57:21 +0900

    211109_fix_origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,8 +13,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true # 要追加
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # credentials: true # 要追加
 
   end
 
