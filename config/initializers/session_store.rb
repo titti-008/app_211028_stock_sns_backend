f838bf6a4acab470df71e6b48bd5883ec4b67512
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-09 13:00:58 +0900

    211109_fix_origins

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === "production"
-  Rails.application.config.session_store :cookie_store, key: '_rails-backend',domain: :all
+  Rails.application.config.session_store :cookie_store, key: '_app-211028-react-frontend',domain: :all
 else
   Rails.application.config.session_store :cookie_store, key: '_rails-backend'
 end
