8721b34050d8dcd791960570f604474092493760
top-note-h <ikuzah_m2005@yahoo.co.jp>
2021-11-09 16:50:59 +0900

    211109_add_testLogin

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 if Rails.env === "production"
   Rails.application.config.session_store :cookie_store, key: '_rails-backend',domain: :all, tld_length: 2
-  Rails.application.config.api_only = false
 else
   Rails.application.config.session_store :cookie_store, key: '_rails-backend'
-  Rails.application.config.api_only = false
 end
